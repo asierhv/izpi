7a4c4ccbcc5e9033f31b724a6e400500b4dd6434
asierhv <asierherranzv@gmail.com>
2025-08-08 14:50:32 +0200

    Fourth commit

--- README.md
+++ README.md
@@ -1,81 +1,99 @@
-# START:
-T1:
-~/izpi:
+# Project Start:
+**Terminal 1**:
+```bash
+cd ~/izpi
 uvicorn api.main:app --reload --port 9000
-
-T2:
-~/izpi/web:
+```
+**Terminal 2**  
+```bash
+cd ~/izpi/web
 python -m http.server 8000
+```
 
-SE:
-http://localhost:8000
+**Site Entry:**  
+[http://localhost:8000](http://localhost:8000)
 
-# Info
-Important public API endpoints:
+---
 
-BASE_URL = https://api.geckoterminal.com/api/v2
-network = solana, eth, ...
-dex = orca, uniswap, raydium, ...
+# API Info
 
-- Get top pools in a network's DEX:
-    endpoint = f"{BASE_URL}/networks/{network}/dexes/{dex}/pools?page=10&sort={sort}"
-    sort = h24_volume_usd_desc, h24_tx_count_desc
+**Base URL:**  
+```
+https://api.geckoterminal.com/api/v2
+```
+- `BASE_URL`: https://api.geckoterminal.com/api/v2
+- `network`: solana, eth, ...
+- `dex`: orca, uniswap, raydium, ...
+
+**Get top pools in a network's DEX**
+```python
+endpoint = f"{BASE_URL}/networks/{network}/dexes/{dex}/pools?page=10&sort={sort}"
+# sort: h24_volume_usd_desc, h24_tx_count_desc
+```
     
-- Get TVL value for up to 30 given pool ids:
-    endpoint = f"{BASE_URL}/networks/{network}/pools/multi/{pool_id_0}%2C{pool_id_1}%2C{pool_id_2}"
-    - Result:
-        tvl = data["data"][n]["attributes"]["reserve_in_usd"]
+**Get TVL value for up to 30 given pool ids**
+```python
+endpoint = f"{BASE_URL}/networks/{network}/pools/multi/{pool_id_0}%2C{pool_id_1}%2C{pool_id_2}"
+# Result:
+# tvl = data["data"][n]["attributes"]["reserve_in_usd"]
+```
 
-- Get ohlcv values for a given pool:
-    endpoint = f"{BASE_URL}/networks/{network}/pools/{pool_id}/ohlcv/{timeframe}?aggregate={aggregate}&limit={limit}&before_timestamp={before_timestamp}&currency={currency}"
-    timeframe = day, hour, minute
-    aggregate = day: 1 - hour: 1, 4, 12 - minute: 1, 5, 15
-    limit: (max 1000)
-    before_timestamp: (seconds in unix format, example: 1679414400)
-    currency: token, usd
+**Get ohlcv values for a given pool**
+```python
+endpoint = f"{BASE_URL}/networks/{network}/pools/{pool_id}/ohlcv/{timeframe}?aggregate={aggregate}&limit={limit}&before_timestamp={before_timestamp}&currency={currency}"
+# timeframe: day, hour, minute
+# aggregate: day: 1 | hour: 1, 4, 12 | minute: 1, 5, 15
+# limit: max 1000
+# before_timestamp: seconds in unix format (e.g. 1679414400)
+# currency: token, usd
+```
 
-- Data structure for pools:  "<pool_address>.json"
+---
+
+## Pool Metadata Structure:  (`<pool_address>.json`)
+```json
 {
     "meta": {
-        "pool_address": str,    # the pool's address
-        "name": str,            # the name of the pool
-        "fee": float,           # the pool's fee in percentage
-        "network": str,         # the network where the pool is (solana, ethereum, arbitrum ...)
-        "dex": str,             # the dex where the pool is (orca, meteora, raydium ...)
+        "pool_address": "str",    // the pool's address
+        "name": "str",            // the name of the pool
+        "fee": "float",           // the pool's fee in percentage
+        "network": "str",         // the network where the pool is (solana, ethereum, arbitrum ...)
+        "dex": "str",             // the dex where the pool is (orca, meteora, raydium ...)
         "base": {
-            "address": str,     # the base token's address
-            "name": str,        # the base token's name
-            "symbol": str       # the base token's symbol
+            "address": "str",     // the base token's address
+            "name": "str",        // the base token's name
+            "symbol": "str"       // the base token's symbol
         },
         "quote": {
-            "address": str,     # the quote token's symbol
-            "name": str,        # the quote token's symbol
-            "symbol": str       # the quote token's symbol
+            "address": "str",     // the quote token's symbol
+            "name": "str",        // the quote token's symbol
+            "symbol": "str"       // the quote token's symbol
         },
-        "pool_created_at": [int, str],
-        "metadata_last_update": [int, str] # the last update date in timestamp unix and iso8601.UTC format
+        "pool_created_at": ["int", "str"],
+        "metadata_last_update": ["int", "str"] // the last update date in timestamp unix and iso8601.UTC format
     },
     "data": [
         {
-            "epoch": [int, str],                                    # timestamp in unix and iso8601.UTC
-            "tvl": int,                                             # the total value locked or liquidity for the pool in $USD
-            "open": [decimal.Decimal, decimal.Decimal],             # the open value in $USD and token value
-            "high": [decimal.Decimal, decimal.Decimal],             # the high value in $USD and token value
-            "low": [decimal.Decimal, decimal.Decimal],              # the low value in $USD and token value
-            "close": [decimal.Decimal, decimal.Decimal],            # the close value in $USD and token value
-            "volume": int,                                          # the volume value in $USD
-            "hour_data": [                                          # the ohlcv data per hour, descendant -> [{23:00}, {22:00}, ..., {00:00}]
+            "epoch": ["int", "str"],                                    // timestamp in unix and iso8601.UTC
+            "tvl": "float",                                             // the total value locked or liquidity for the pool in $USD
+            "open": ["float", "float"],             // the open value in $USD and token value
+            "high": ["float", "float"],             // the high value in $USD and token value
+            "low": ["float", "float"],              // the low value in $USD and token value
+            "close": ["float", "float"],            // the close value in $USD and token value
+            "volume": "float",                                          // the volume value in $USD
+            "hour_data": [                                          // the ohlcv data per hour, descendant -> [{23:00}, {22:00}, ..., {00:00}]
                 {
-                    "epoch": [int, str],                            # timestamp in unix and iso8601.UTC
-                    "open": [decimal.Decimal, decimal.Decimal],     # the open value in $USD and token value
-                    "high": [decimal.Decimal, decimal.Decimal],     # the high value in $USD and token value
-                    "low": [decimal.Decimal, decimal.Decimal],      # the low value in $USD and token value
-                    "close": [decimal.Decimal, decimal.Decimal],    # the close value in $USD and token value
-                    "volume": int,                                  # the volume value in $USD
+                    "epoch": ["int", "str"],                            // timestamp in unix and iso8601.UTC
+                    "open": ["float", "float"],     // the open value in $USD and token value
+                    "high": ["float", "float"],     // the high value in $USD and token value
+                    "low": ["float", "float"],      // the low value in $USD and token value
+                    "close": ["float", "float"],    // the close value in $USD and token value
+                    "volume": "float",                                  // the volume value in $USD
                 },
-                ...
+                //...
             ]
         },
-        ...
+        //...
     ] 
 }
+```
--- metadata_scrapper.py
+++ metadata_scrapper.py
@@ -18,9 +18,9 @@ def call_get_request(endpoint):
     retries = 0
     while retries < max_retries:
         if CALLS_COUNTER >= 30:
-            print("Limit of 30 calls/min reached, waiting for 65s")
+            print(" Limit of 30 calls/min reached, waiting for 65s")
             for i in range(65, 0, -1):
-                print(f"{i}s", end="\r")
+                print(f"{i}s ", end="\r")
                 time.sleep(1)
             CALLS_COUNTER = 0
         response = requests.get(endpoint)
@@ -29,10 +29,10 @@ def call_get_request(endpoint):
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 401:
-            print("Data not available for input timestamp")
+            print(" Data not available for input timestamp")
             return None
         retries += 1
-        print(f"\nRetry {retries}/{max_retries}: Error {response.status_code}, retrying...")
+        print(f" \nRetry {retries}/{max_retries}: Error {response.status_code}, retrying...")
 
 ####################################################################################################
 #########################################  GET - FUNCTIONS  ########################################
@@ -41,7 +41,7 @@ def call_get_request(endpoint):
 def get_top_pools_info(network, dex, top_pools_info=None, sort: str="default"):
     # Get top 200 pools on dex from geckoterminal, sorted by default (trend), volume_usd or tx_count
     pools_info = []
-    print(f"Getting top 200 pools on {network}/{dex}")
+    print(f" Getting top 200 pools on {network}/{dex}")
     for n in tqdm(range(10)):
         endpoint = f"https://api.geckoterminal.com/api/v2/networks/{network}/dexes/{dex}/pools?page={n+1}"
         if sort in ("h24_volume_usd_desc","h24_tx_count_desc"):
@@ -61,10 +61,10 @@ def get_top_pools_info(network, dex, top_pools_info=None, sort: str="default"):
                     pools_info.append(entry_dict)
                 else:
                     repeated_pools+=1
-                    print("REPEATED POOL:", entry_dict)
+                    print(" REPEATED POOL:", entry_dict)
             else:
                 pools_info.append(entry_dict)
-    print("Total Repeated Pools:",repeated_pools)
+    print(" Total Repeated Pools:",repeated_pools)
     
     # Writes or updates the total_top_pools.json file with the new pools
     if top_pools_info == None:
@@ -133,22 +133,6 @@ def get_ohlcv_info(pool_info, before_timestamp, limit: int=1000, timeframe: str=
     response_data = call_get_request(endpoint)
     return response_data
 
-def get_tvl_info(top_pools_info):
-    # Get TVL data for available pools, groups them in calls of 30 pools to the geckoterminal API
-    available_pools_info = [item for item in top_pools_info if item["tvl_history_available"]==True]
-    pool_batch_list = [available_pools_info[i:i+30] for i in range(0, len(available_pools_info), 30)]
-    network = available_pools_info[0]["network"]
-    pools_tvl_info = {}
-    for pool_batch in pool_batch_list:
-        endpoint = f"https://api.geckoterminal.com/api/v2/networks/{network}/pools/multi/"
-        for pool in pool_batch:
-            endpoint = endpoint + f"{pool['address']}%2C"
-        endpoint = endpoint[:-3]
-        response_data = call_get_request(endpoint)
-        for tvl_info in response_data["data"]:
-            pools_tvl_info[tvl_info["attributes"]["address"]] = float(tvl_info["attributes"]["reserve_in_usd"])
-    return pools_tvl_info
-
 ####################################################################################################
 ##################################  CREATE / UPDATE - FUNCTIONS  ###################################
 ####################################################################################################
@@ -162,38 +146,40 @@ def create_pool_metadata(pool_info, query_data_dict, utc_now):
     json_filepath = f"./metadata/pools/pools_metadata/{pool_info['address']}.json"
 
     if os.path.exists(json_filepath):
-        print(f"Updating metadata for pool {pool_info['name']} ({pool_info['address']})")
+        print(f" Updating metadata for pool {pool_info['name']} ({pool_info['address']})")
         with open(json_filepath, "r", encoding="utf-8") as json_infile:
             metadata = json.load(json_infile)
         utc_metadata_last_update = datetime.fromtimestamp(metadata["meta"]["metadata_last_update"][0], tz=timezone.utc)
         if utc_metadata_last_update >= utc_midnight:
-            print(f"Pool {pool_info['name']} ({pool_info['address']}) metadata is already up to date.")
+            print(f" Metadata for pool {pool_info['name']} ({pool_info['address']}) is already up to date.")
             return
     else:
-        print(f"Creating metadata for pool {pool_info['name']} ({pool_info['address']})")
+        print(f" Creating metadata for pool {pool_info['name']} ({pool_info['address']})")
 
     # Calls for getting the days data
     response_data_usd = get_ohlcv_info(pool_info, before_timestamp=timestamp, currency="usd")
     response_data_token = get_ohlcv_info(pool_info, before_timestamp=timestamp, currency="token")
-    meta = {
-        "pool_address": pool_info["address"],      # the pool's address
-        "name": pool_info["name"],                 # the pool's name
-        "fee": "<EMPTY>",                          # the pool's fee in percentage (must be searched manually)
-        "network": pool_info["network"],           # the network where the pool is (solana, ethereum, arbitrum ...)
-        "dex": pool_info["dex"],                   # the dex where the pool is (orca, meteora, raydium ...)
-        "base": {
-            "address": response_data_usd["meta"]["base"]["address"],     # the base token's address
-            "name": response_data_usd["meta"]["base"]["name"],           # the base token's name
-            "symbol": response_data_usd["meta"]["base"]["symbol"]        # the base token's symbol
-        },
-        "quote": {
-            "address": response_data_usd["meta"]["quote"]["address"],    # the quote token's address
-            "name": response_data_usd["meta"]["quote"]["name"],          # the quote token's name
-            "symbol": response_data_usd["meta"]["quote"]["symbol"]       # the quote token's symbol
-        },
-        "pool_created_at": [int(datetime.fromisoformat(pool_info["pool_created_at"]).timestamp()), pool_info["pool_created_at"]],  # the pool creation time in timestamp unix and iso8601.UTC format
-        "metadata_last_update": [int(utc_now.timestamp()), utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')]      # the last update in timestamp unix and iso8601.UTC format
-    }
+    if not os.path.exists(json_filepath):
+        meta = {
+            "pool_address": pool_info["address"],      # the pool's address
+            "name": pool_info["name"],                 # the pool's name
+            "fee": "<EMPTY>",                          # the pool's fee in percentage (must be searched manually)
+            "network": pool_info["network"],           # the network where the pool is (solana, ethereum, arbitrum ...)
+            "dex": pool_info["dex"],                   # the dex where the pool is (orca, meteora, raydium ...)
+            "base": {
+                "address": response_data_usd["meta"]["base"]["address"],     # the base token's address
+                "name": response_data_usd["meta"]["base"]["name"],           # the base token's name
+                "symbol": response_data_usd["meta"]["base"]["symbol"]        # the base token's symbol
+            },
+            "quote": {
+                "address": response_data_usd["meta"]["quote"]["address"],    # the quote token's address
+                "name": response_data_usd["meta"]["quote"]["name"],          # the quote token's name
+                "symbol": response_data_usd["meta"]["quote"]["symbol"]       # the quote token's symbol
+            },
+            "pool_created_at": [int(datetime.fromisoformat(pool_info["pool_created_at"]).timestamp()), pool_info["pool_created_at"]],  # the pool creation time in timestamp unix and iso8601.UTC format
+            "metadata_last_update": [int(utc_now.timestamp()), utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')]      # the last update in timestamp unix and iso8601.UTC format
+        }
+
     data = []
     for i, ohlcv_usd_entry in enumerate(response_data_usd["data"]["attributes"]["ohlcv_list"]):
         ohlcv_token_entry = response_data_token["data"]["attributes"]["ohlcv_list"][i]
@@ -236,7 +222,7 @@ def create_pool_metadata(pool_info, query_data_dict, utc_now):
 
     if os.path.exists(json_filepath):
         existing_epoch_list = [item["epoch"][0] for item in metadata["data"]]
-        new_data = [item for item in data if item["epoch"][0] not in existing_epoch_list and not int(utc_midnight.timestamp()) == item["epoch"][0]] # Only add the new day data and remove the actual day data (because it's not completed for 24h)
+        new_data = [item for item in data if item["epoch"][0] not in existing_epoch_list and not int(utc_midnight.timestamp()) == item["epoch"][0]] # Only add the new days' data and remove the actual day's data (because it's not completed for 24h)
         metadata["data"] = new_data + metadata["data"]
         metadata["meta"]["metadata_last_update"] = [int(utc_now.timestamp()), utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')]
     else:
@@ -245,60 +231,14 @@ def create_pool_metadata(pool_info, query_data_dict, utc_now):
 
     with open(json_filepath, "w", encoding="utf-8") as json_file:
         json.dump(metadata, json_file, ensure_ascii=False)
-  
-# Need to update this function to remove the first item, similar to the create_pool_metadata function
-def daily_update_pool_metadata(pool_info, utc_now, pools_tvl_info):
-    # Updates the ohlcv and tvl data for existing and available pools, for the past day
-    utc_midnight = datetime(utc_now.year, utc_now.month, utc_now.day, 0, 0, 0, tzinfo=timezone.utc)
-    timestamp = int(utc_midnight.timestamp())
-    json_filepath = f"./metadata/pools/pools_metadata/{pool_info['address']}.json"
-    print(f"Updating daily metadata for pool {pool_info['name']} ({pool_info['address']})")
-    with open(json_filepath, "r", encoding="utf-8") as json_infile:
-        metadata = json.load(json_infile)
-
-    # Calls for getting the day data
-    response_data_usd = get_ohlcv_info(pool_info, limit=2, before_timestamp=timestamp, currency="usd")
-    response_data_token = get_ohlcv_info(pool_info, limit=2, before_timestamp=timestamp, currency="token")
-    ohlcv_usd_entry = response_data_usd["data"]["attributes"]["ohlcv_list"][1]
-    ohlcv_token_entry = response_data_token["data"]["attributes"]["ohlcv_list"][1]
-    epoch = datetime.fromtimestamp(ohlcv_usd_entry[0], tz=timezone.utc)
-    day_item = {
-        "epoch": [int(epoch.timestamp()), epoch.strftime('%Y-%m-%dT%H:%M:%SZ')],
-        "tvl": pools_tvl_info[pool_info["address"]],
-        "open": [ohlcv_usd_entry[1], ohlcv_token_entry[1]],
-        "high": [ohlcv_usd_entry[2], ohlcv_token_entry[2]],
-        "low": [ohlcv_usd_entry[3], ohlcv_token_entry[3]],
-        "close": [ohlcv_usd_entry[4], ohlcv_token_entry[4]],
-        "volume": ohlcv_usd_entry[5],
-        "hour_data": []
-    }
-
-    # Calls for getting the hours data
-    response_data_usd = get_ohlcv_info(pool_info, limit=25, before_timestamp=timestamp, timeframe="hour", currency="usd")
-    response_data_token = get_ohlcv_info(pool_info, limit=25, before_timestamp=timestamp, timeframe="hour", currency="token")
-    for i, ohlcv_usd_entry in enumerate(response_data_usd["data"]["attributes"]["ohlcv_list"][1:]):
-        ohlcv_token_entry = response_data_token["data"]["attributes"]["ohlcv_list"][i+1]
-        epoch = datetime.fromtimestamp(ohlcv_usd_entry[0], tz=timezone.utc)
-        hour_item = {
-            "epoch": [int(epoch.timestamp()), epoch.strftime('%Y-%m-%dT%H:%M:%SZ')],
-            "open": [ohlcv_usd_entry[1], ohlcv_token_entry[1]],
-            "high": [ohlcv_usd_entry[2], ohlcv_token_entry[2]],
-            "low": [ohlcv_usd_entry[3], ohlcv_token_entry[3]],
-            "close": [ohlcv_usd_entry[4], ohlcv_token_entry[4]],
-            "volume": ohlcv_usd_entry[5]
-        }
-        day_item["hour_data"].append(hour_item)
-    
-    metadata["meta"]["metadata_last_update"] = [int(utc_now.timestamp()), utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')]
-    metadata["data"].insert(0,day_item)
-    with open(json_filepath, "w", encoding="utf-8") as json_outfile:
-        json.dump(metadata, json_outfile, ensure_ascii=False)
 
 ####################################################################################################
 ##############################################  MAIN  ##############################################
 ####################################################################################################
 
 def pools_creation(network, dex, ignore_steps=None):
+    # Creates the metadata for all pools in a given newtork and dex
+    print("\n------ POOLS CREATION ------\n")
     with open("./keys/dune_api_key", "r", encoding="utf-8") as f:
         dune_api_key = f.read().strip()
     utc_now = datetime.now(timezone.utc)
@@ -311,40 +251,33 @@ def pools_creation(network, dex, ignore_steps=None):
     
     # Step A: Get top pools info from geckoterminal
     if not "A" in ignore_steps:
-        print(f"Step A: Getting top pools info for {network}/{dex}")
+        print(f" Step A: Getting top pools info for {network}/{dex}...")
         top_pools_info = get_top_pools_info(network, dex, top_pools_info=top_pools_info)
         top_pools_info = get_top_pools_info(network, dex, top_pools_info=top_pools_info, sort="h24_volume_usd_desc")
         top_pools_info = get_top_pools_info(network, dex, top_pools_info=top_pools_info, sort="h24_tx_count_desc")
     elif "A" in ignore_steps:
-        print(f"Skipping Step A: Using existing top pools info")     
+        print(f" Skipping Step A: Using existing top pools info.")     
 
     # Step B: Get dune query data
     if not "B" in ignore_steps:
-        print(f"Step B: Getting dune query data for {network}/{dex}")
+        print(f" Step B: Getting dune query data for top pools in {network}/{dex}...")
         query_data_dict, top_pools_info = get_dune_query_data(dune_api_key, query_id, top_pools_info)
     elif "B" in ignore_steps:
-        print(f"Skipping Step B: Using existing dune query data")
+        print(f" Skipping Step B: Using existing dune query data.")
         with open("./metadata/queries/dune_query_result.json", "r", encoding="utf-8") as f:
             query_data_dict = json.load(f)
 
     # Step C: Create pool metadata
+    print(f" Step C: Creating pools metadata for top pools in {network}/{dex}...")
     for pool_info in tqdm(top_pools_info):
         if pool_info["tvl_history_available"]:
             create_pool_metadata(pool_info, query_data_dict, utc_now)
         else:
             print("TVL history not available for pool:", pool_info["name"], pool_info["address"])
 
-def pools_daily_update():
-    utc_now = datetime.now(timezone.utc)
-    with open("./metadata/pools/top_pools_info.json", "r", encoding="utf-8") as f:
-        top_pools_info = [json.loads(line) for line in f]
-    pools_tvl_info = get_tvl_info(top_pools_info)
-    for pool_info in tqdm(top_pools_info):
-        if pool_info["tvl_history_available"]:
-            daily_update_pool_metadata(pool_info, utc_now, pools_tvl_info)
-
 if __name__ == "__main__":
     network = "solana"
     dex = "orca"
     ignore_steps = ["A","B"]
+    # pools_creation(network, dex)
     pools_creation(network, dex, ignore_steps)
