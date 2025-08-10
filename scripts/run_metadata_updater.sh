56f7755027ce3bf92b1043b40d8a8ed20d4904e8
asierhv <asierherranzv@gmail.com>
2025-08-10 18:45:47 +0000

    Auto-update: 2025-08-10T18:45:47Z

--- scripts/run_metadata_updater.sh
+++ scripts/run_metadata_updater.sh
@@ -11,7 +11,7 @@ LOGFILE="logs/metadata_updater_$(date -u +'%Y-%m-%dT%H:%M:%SZ').log"
 
 # Run the updater and log output
 echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Running metadata_updater.py..." >> "$LOGFILE"
-python3 metadata_updater.py >> "$LOGFILE" 2>&1
+python3 scripts/metadata_updater.py >> "$LOGFILE" 2>&1
 
 # Add and commit changes
 echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Pushing changes to GitHub..." >> "$LOGFILE"
